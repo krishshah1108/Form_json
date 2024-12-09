fc1bceceb2fb434c0d2d01c843b30fff661f73df
krishshah1108 <krishhshah1108@gmail.com>
2024-12-09 17:44:08 +0530

    hadndled eventlistener

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-import createForm from '../lib/form.js';
-import formData from '../data/formData.js';
+import createForm from "../lib/form.js";
+import formData from "../data/formData.js";
 
-const form = createForm(formData);
-const container = document.getElementById('container');
+const { form, submitData, resetData } = createForm(formData);
+const container = document.getElementById("container");
 container.appendChild(form);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = submitData();
+  console.log(data);
+  resetData();
+});
+
+form.addEventListener("reset", (e) => {
+  e.preventDefault();
+  resetData();
+});
