b682569acaeb59fe1475a8630c45ae850ffd1e7f
krishshah1108 <krishhshah1108@gmail.com>
2024-12-10 19:14:55 +0530

    created saveData

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-import createForm from "../lib/form.js";
-import formData from "../data/formData.js";
+import { createForm, getFormData, resetFormData } from '../lib/form.js';
+import formData from '../data/formData.js';
+import { saveData } from '../lib/storage.js';
 
-const { form, submitData, resetData } = createForm(formData);
-const container = document.getElementById("container");
+const form = createForm(formData);
+const container = document.getElementById('container');
 container.appendChild(form);
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const data = submitData();
+  const data = getFormData(form);
   console.log(data);
-  resetData();
+  saveData(data);
+  resetFormData(form);
 });
 
-form.addEventListener("reset", (e) => {
+form.addEventListener('reset', (e) => {
   e.preventDefault();
-  resetData();
+  resetFormData(form);
 });
